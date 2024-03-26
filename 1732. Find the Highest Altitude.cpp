7e216a36ff05ded6be0ada6b96f50d2c005e5054
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-03-26 19:49:58 +0530

    .

---
+++ 1732. Find the Highest Altitude.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int sum=0;
+        int max_sum=0;
+        for(int i=0;i<gain.size();i++)
+        {
+            sum+=gain[i];
+            max_sum=max(max_sum,sum);
+        }
+        return max_sum;
+    }
+};
