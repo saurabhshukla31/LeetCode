f541385706c74276e41e467e4aa5118a22b93a6b
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-03-28 00:25:23 +0530

    .

--- 231. Power of Two.cpp
+++ 231. Power of Two.cpp
@@ -1,20 +1,21 @@
-#include <cmath>
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int x;
-        for(int i=0;i<31;i++)
-        {
-            x=pow(2,i);
-            if(x==n)
-            {
+        while (true) {
+            if (n > 0 && n % 2 == 0) {
+                n = n / 2;
+            }
+            if (n == 1) {
                 return true;
             }
-            else if(x>n)
+            else if(n!=1 && n%2!=0)
+            {
+                return false;
+            }
+            else if(n==0 || n<0)
             {
                 return false;
             }
         }
-        return false;
     }
 };
