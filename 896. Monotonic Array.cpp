71b711f9b9e7aa01123484c62de386c2e87c8a2e
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-04-01 00:42:04 +0530

    .

--- 896. Monotonic Array.cpp
+++ 896. Monotonic Array.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        vector<int> temp(nums);
         bool flag1 = true;
         bool flag2 = true;
         for (int i = 0; i < nums.size() - 1; i++) {
@@ -9,8 +8,8 @@ public:
                 flag1 = false;
             }
         }
-        for (int i = 0; i < temp.size() - 1; i++) {
-            if (temp[i] > temp[i + 1]) {
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
                 flag2 = false;
             }
         }
