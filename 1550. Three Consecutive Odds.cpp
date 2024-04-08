d5edc3922c21763607a2cfc5720494fdd9cdf3ac
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-04-09 00:21:33 +0530

    .

---
+++ 1550. Three Consecutive Odds.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n=arr.size();
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]%2!=0)
+            {
+                count++;
+                if(count==3)
+                {
+                    break;
+                }
+            }
+            else if(arr[i]%2==0)
+            {
+                count=0;
+            }
+        }
+        if(count==3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
