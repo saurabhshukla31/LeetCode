c51ea805c0c50472fa89d361c013c135ec560686
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-04-23 20:45:21 +0530

    .

---
+++ 3099. Harshad Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum=0;
+        int temp=x;
+        while(x!=0)
+        {
+            int rem=x%10;
+            sum=sum+rem;
+            x=x/10;
+        }
+        if(temp%sum==0)
+        {
+            return sum;
+        }
+        return -1;
+    }
+};
