a15b0ea356b4c821bd31cd0c4a1e6c110878b7b9
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-05-09 17:32:10 +0530

    .

--- 657. Robot Return to Origin.cpp
+++ 657. Robot Return to Origin.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        int U = 0, D = 0, R = 0, L = 0;
-        for(int i = 0; i < moves.size(); i++) {
-            if(moves[i] == 'U')
+        int U = 0;
+        int D = 0;
+        int L = 0;
+        int R = 0;
+        for (int i = 0; i < moves.size(); i++) {
+            if (moves[i] == 'U') {
                 U++;
-            else if(moves[i] == 'D')
+            } else if (moves[i] == 'D') {
                 D++;
-            else if(moves[i] == 'R')
-                R++;
-            else if(moves[i] == 'L')
+            } else if (moves[i] == 'L') {
                 L++;
+            } else {
+                R++;
+            }
         }
-        return (U == D && R == L);
+        return U == D && L == R;
     }
 };
