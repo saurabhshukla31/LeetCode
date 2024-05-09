5ddf219b77c611b88e00fb2eb471f09024b220ad
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-05-09 18:43:25 +0530

    .

--- 1492. The kth Factor of n.cpp
+++ 1492. The kth Factor of n.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     int kthFactor(int n, int k) {
         vector<int> ans;
-        int j = 0;
         for (int i = 1; i <= 1000; i++) {
             if (n % i == 0) {
                 ans.push_back(i);
-                j++;
             }
         }
-        if(j<k)
-        {
+        if (ans.size() < k) {
             return -1;
-        }
-        else
-        {
-            return ans[k-1];
+        } else {
+            return ans[k - 1];
         }
     }
 };
