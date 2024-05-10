6103ebe27ac2d0f81d8216f093259922c2ea6147
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-05-10 21:52:10 +0530

    .

---
+++ 1009. Complement of Base 10 Integer.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0)
+            return 1;
+
+        vector<int> binary;
+
+        while (n != 0) {
+            binary.push_back(n % 2);
+            n /= 2;
+        }
+
+        for (int i = 0; i < binary.size(); i++) {
+            if (binary[i] == 0) {
+                binary[i] = 1;
+            } else {
+                binary[i] = 0;
+            }
+        }
+
+        int ans = 0;
+        int p = 0;
+        for (int i = 0; i < binary.size(); i++) {
+            ans += binary[i] * pow(2, p);
+            p++;
+        }
+        return ans;
+    }
+};
