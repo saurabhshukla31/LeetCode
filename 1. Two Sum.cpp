d6a16d7f1e6c2a03e9c2b6e10b62322540aa9480
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-05-10 14:23:40 +0530

    .

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) 
-    {   int n=nums.size();
-        vector<int>ans;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(nums[i]+nums[j]==target)
-                {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> temp(n);
+        temp = nums;
+        sort(nums.begin(), nums.end());
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] == target) {
+                break;
+            } else if (nums[left] + nums[right] < target) {
+                left++;
+            } else {
+                right--;
             }
         }
-        return ans;
+        int ans1 = -1;
+        int ans2 = -1;
+        for (int i = 0; i < n; i++) {
+            if (temp[i] == nums[left] && ans1 == -1) {
+                ans1 = i;
+            } else if (temp[i] == nums[right] && ans2 == -1) {
+                ans2 = i;
+            }
+        }
+        return {ans1, ans2};
     }
 };
