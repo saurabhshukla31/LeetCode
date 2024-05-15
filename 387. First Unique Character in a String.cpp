7c7a2ede044c3ee3fe03c3cc68c03445d0bc60bd
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-05-15 22:22:05 +0530

    .

---
+++ 387. First Unique Character in a String.cpp
@@ -0,0 +1,21 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        for (int i = 0; i < s.size(); i++) {
+            bool isUnique = true;
+            for (int j = 0; j < s.size(); j++) {
+                if (i != j && s[i] == s[j]) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
