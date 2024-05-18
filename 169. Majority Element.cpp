a2d2e80f4712354762aaa6beb1fa1a88e245b4df
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-05-18 17:13:24 +0530

    .

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) 
-    {
-        int n=nums.size();
-        int ans;
-        sort(nums.begin(),nums.end());
-        ans = nums[n/2];
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int count = 0;
+        int max_count = 0;
+        int ans = 0;
+        if (n == 1) {
+            return nums[0];
+        }
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                count = 0;
+            }
+            if (count > max_count) {
+                max_count = count;
+                ans = nums[i];
+            }
+        }
         return ans;
     }
 };
