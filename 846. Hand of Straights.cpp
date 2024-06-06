e4dd09ce5e5c3121b7f42e72af973e6c03d17551
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-06-06 14:30:55 +0530

    .

---
+++ 846. Hand of Straights.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (n % k != 0) {
+            return false;
+        }
+        map<int, int> mpp;
+        for (int num : nums) {
+            mpp[num]++;
+        }
+        for (auto [num, freq] : mpp) {
+            while (freq--) {
+                for (int curr = num; curr < num + k; curr++) {
+                    if (mpp[curr] == 0) {
+                        return false;
+                    } else {
+                        mpp[curr]--;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
