be36573255bee495c130ed386c3db209075308bc
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-06-12 10:59:48 +0530

    .

--- 75. Sort Colors.cpp
+++ 75. Sort Colors.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n=nums.size();
-        int low=0;
-        int mid=0;
-        int high=n-1;
-        while(mid<=high)
-        {
-            if(nums[mid]==0)
-            {
-                swap(nums[mid],nums[low]);
-                low++;
-                mid++;
+        int p1 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                swap(nums[p1], nums[i]);
+                p1++;
             }
-            else if (nums[mid]==1)
-            {
-                mid++;
-            }
-            else
-            {
-                swap(nums[mid],nums[high]);
-                high--;
+        }
+        int p2 = p1;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] == 1) {
+                swap(nums[p2], nums[j]);
+                p2++;
             }
         }
     }
